61fbac690dd1631716ebb31fcd391aa2c0860a0e
Navya Mallidi <mallidinavya635@gmail.com>
2024-06-02 20:18:56 +0530

    WAN

---
+++ app/utils/defaultStates.tsx
@@ -0,0 +1,39 @@
+const defaultStates = [
+  {
+    name: "Delhi",
+    country: "IN",
+    state: "Delhi",
+    lat: 28.7041,
+    lon: 77.1025,
+  },
+  {
+    name: "Mumbai",
+    country: "IN",
+    state: "Maharashtra",
+    lat: 19.076,
+    lon: 72.8777,
+  },
+  {
+    name: "Bangalore",
+    country: "IN",
+    state: "Karnataka",
+    lat: 12.9716,
+    lon: 77.5946,
+  },
+  {
+    name: "Kolkata",
+    country: "IN",
+    state: "West Bengal",
+    lat: 22.5726,
+    lon: 88.3639,
+  },
+  {
+    name: "Chennai",
+    country: "IN",
+    state: "Tamil Nadu",
+    lat: 13.0827,
+    lon: 80.2707,
+  },
+];
+
+export default defaultStates;
